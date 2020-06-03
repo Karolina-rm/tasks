246c42bb1d343ab9eda28e1a4cac36727755ae39
Karolina-rm <karolina.ra.kr@gmail.com>
2020-06-03 08:06:37 +0200

    task 29.1 solution

--- src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.nio.channels.FileChannel;
 import java.util.List;
 
 @Getter
@@ -22,4 +23,5 @@ public class TrelloBoardDto {
 
     @JsonProperty("lists")
     private List<TrelloListDto> lists;
+
 }
